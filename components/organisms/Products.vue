1181d00db477496f75e6071d846380e1a88833f2
Rafael Santos <r12rafasantos@gmail.com>
2023-03-10 17:00:43 -0300

    Ajuste na home

--- components/organisms/Products.vue
+++ components/organisms/Products.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-screen-xl m-auto mt-14">
+  <div class="max-w-screen-xl m-auto mt-14 mb-8">
     <div class="mx-24">
       <AtomsTitle tag="h2" fontSize="34px" fontWeight="800">
         Conheça nossos outros produtos
@@ -16,6 +16,73 @@
               Aproveite nossos pacotes promocionais com ônibus + hotel com café
               da manhã!
             </span>
+            <AtomsButtonCustom
+              class="mt-6"
+              color="#fff"
+              bgColor="#FFC107"
+              borderColor="#FFC107"
+              >Comprar</AtomsButtonCustom
+            >
+          </div>
+        </template>
+      </MoleculesCardProducts>
+      <MoleculesCardProducts>
+        <template v-slot:content>
+          <div class="body-card mt-6">
+            <AtomsTitle tag="h2" fontSize="22px" fontWeight="700" color="#fff">
+              Garanta seu Pacote de viagem
+            </AtomsTitle>
+            <span class="text-sm text-white font-semibold text-xs">
+              Aproveite nossos pacotes promocionais com ônibus + hotel com café
+              da manhã!
+            </span>
+            <AtomsButtonCustom
+              class="mt-6"
+              color="#fff"
+              bgColor="#FFC107"
+              borderColor="#FFC107"
+              >Comprar</AtomsButtonCustom
+            >
+          </div>
+        </template>
+      </MoleculesCardProducts>
+      <MoleculesCardProducts>
+        <template v-slot:content>
+          <div class="body-card mt-6">
+            <AtomsTitle tag="h2" fontSize="22px" fontWeight="700" color="#fff">
+              Garanta seu Pacote de viagem
+            </AtomsTitle>
+            <span class="text-sm text-white font-semibold text-xs">
+              Aproveite nossos pacotes promocionais com ônibus + hotel com café
+              da manhã!
+            </span>
+            <AtomsButtonCustom
+              class="mt-6"
+              color="#fff"
+              bgColor="#FFC107"
+              borderColor="#FFC107"
+              >Comprar</AtomsButtonCustom
+            >
+          </div>
+        </template>
+      </MoleculesCardProducts>
+      <MoleculesCardProducts>
+        <template v-slot:content>
+          <div class="body-card mt-6">
+            <AtomsTitle tag="h2" fontSize="22px" fontWeight="700" color="#fff">
+              Garanta seu Pacote de viagem
+            </AtomsTitle>
+            <span class="text-sm text-white font-semibold text-xs">
+              Aproveite nossos pacotes promocionais com ônibus + hotel com café
+              da manhã!
+            </span>
+            <AtomsButtonCustom
+              class="mt-6"
+              color="#fff"
+              bgColor="#FFC107"
+              borderColor="#FFC107"
+              >Comprar</AtomsButtonCustom
+            >
           </div>
         </template>
       </MoleculesCardProducts>
